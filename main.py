468e5da963ae7a408d6c61c57ce84ecc84e0ecb2
arinavis <avisnevskaja@gmail.com>
2024-10-01 19:19:47 +0300

    Atliktos 3 užduotys

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # append priklijuoja į galą
 # extend priklijuoja daugiau negu viena
-# insert prilijuoja į norimą vietą
+# insert priklijuoja į norimą vietą
 
 names = [
     'Jonas',
@@ -24,15 +24,37 @@ print(vardai[3])
 print(len(vardai))
 print()
 # 2 uzd.
-ugis =[1.65, 1.90, 1.70, 1.83, 1.55, 1.89]
+ugis = [1.65, 1.90, 1.70, 1.83, 1.55, 1.89]
 print(ugis)
 print(len(ugis))
 print()
 # 3 uzd.
 
-markList = input('Įveskite pažymius: ')
-pazymiai = list(map(int, markList.split()))
-print(pazymiai)
-print(len(pazymiai))
+# markList = input('Įveskite pažymius: ')
+# pazymiai = list(map(int, markList.split()))
+# print(pazymiai)
+# print(len(pazymiai))
 # print(list(map(int, markList.split())))
 
+# chatGPT variantas
+
+# pazSarasas = input('Įveskite pažymius: ')
+# print(pazSarasas.split(','))
+# print(len(pazSarasas.split(',')))
+
+# 4 uzd.
+# miestai = ['Vilnius', 'Kaunas', 'Zarasai', 'Visaginas']
+# print(miestai)
+# naujas_miestas = input('Įveskite miestą: ')
+# miestai.append(naujas_miestas)
+# print(miestai)
+# miestai.insert(0, naujas_miestas)
+# print(miestai)
+
+# pavyko
+
+
+
+
+# 5 uzd
+list = [8, 5, 25, 69, 7, 10]
